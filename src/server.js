630d97f8d63ff1bdf47198e6a605dc7c4baf9aea
elias.alexandre <elliasallex@gmail.com>
2023-02-13 12:01:31 -0300

    request body on JSON (stream & Buffers)

--- src/server.js
+++ src/server.js
@@ -1,15 +1,39 @@
 import http from "http";
 
-const server = http.createServer((req, res) => {
+const users = []
+
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch (error) {
+    req.body = null
+  }
+
 
   if (method === "GET" && url === "/users") {
     // Early return
-    return res.end("Listagem de usuários");
+    return res.setHeader('Content-type', 'application/json').end(JSON.stringify(users));
   }
 
+
   if (method === "POST" && url === "/users") {
-    return res.end("Criação de usuários");
+    const { name, email } = req.body
+
+    users.push({
+      id: 1,
+      name,
+      email,
+    })
+
+    return res.writeHead(202).end()
   }
 
   return res.end("Hello world");
