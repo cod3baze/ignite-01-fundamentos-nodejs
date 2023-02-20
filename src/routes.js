1520ce44e4e285731201ea90c770aaa5e9cc75d0
elias.alexandre <elliasallex@gmail.com>
2023-02-20 09:13:17 -0300

    Route with params (RegEx)

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { randomUUID } from "node:crypto";
 import { Database } from "./database.js";
+import { buildRoutePath } from "./utils/build-route-path.js";
 
 const database = new Database();
 
 export const routes = [
   {
     method: "GET",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: async (req, res) => {
       // Early return
       return res
@@ -16,7 +17,7 @@ export const routes = [
   },
   {
     method: "POST",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: async (req, res) => {
       const { name, email } = req.body;
 
@@ -31,7 +32,7 @@ export const routes = [
   },
   {
     method: "DELETE",
-    path: "/users/{id}",
+    path: buildRoutePath("/users/:id"),
     handler: async (req, res) => {
       const { id } = req.params;
 
